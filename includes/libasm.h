31b035a3a0fd4432c13ded43aed1fc574f4f50a3
Alex Cardona <42.alex.cardona@gmail.com>
2025-01-28 14:35:52 +0100

    fix: restricting header to mandatory part

--- includes/libasm.h
+++ includes/libasm.h
@@ -10,11 +10,4 @@ ssize_t	ft_read(int fd, void *buf, size_t count);
 ssize_t	ft_write(int fd, const void *buf, size_t count);
 char	*ft_strdup(const char *s);
 
-
-typedef struct s_list
-{
-	void			*data;
-	struct s_list	*next;
-}	t_list;
-
 #endif
