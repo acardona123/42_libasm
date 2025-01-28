89cfe41b9b265ad684858455b6a4e6aeb5f5b9e5
Alex Cardona <42.alex.cardona@gmail.com>
2025-01-28 17:00:46 +0100

    doc: adding comments about stack alignment

--- srcs/mandatory/ft_strdup.s
+++ srcs/mandatory/ft_strdup.s
@@ -1,5 +1,5 @@
 section .bss
-	dest RESD 1
+	dest RESD 1 ; static (because not global) var stored in the program data segment (lifetime of the entire pgm). Not optimized here as registers could have been used (faster and less resources) (only pedagogic use)
 	
 section .text
 
@@ -17,6 +17,10 @@ ft_strdup:
 .allocate_mem:
 	inc rax
 	mov rdi, rax
+	;checking stack alignment:
+		;after ft_strdup call the stack was 16 - 8 bytes aligned (return pointer pushed to the stack)
+		;after push rpb we are 8-8o aligned which is 16o aligned
+		;=> alignement ok
 	call qword [rel malloc wrt ..got]
 	cmp rax, byte 0
 	je .return
@@ -26,4 +30,5 @@ ft_strdup:
 	call qword [rel ft_strcpy wrt ..got]
 .return:
 	pop rbp
+	add rsp , 4; de-allocate mem of local var dest
 	ret
