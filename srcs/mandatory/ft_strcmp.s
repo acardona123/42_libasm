695f6370449d3dc162a59ff45d9e6ae81df8f9c7
Alex Cardona <42.alex.cardona@gmail.com>
2025-03-03 21:51:37 +0100

    refactor: avoid tmp register

--- srcs/mandatory/ft_strcmp.s
+++ srcs/mandatory/ft_strcmp.s
@@ -10,20 +10,20 @@ ft_strcmp:
 	xor rax, rax; used as an index i
 
 .cmp_char:
-	movzx	r10, byte [rsi + rax]; s1[i] saved in reg for cmp 
+	movzx	r8, byte [rsi + rax]; s1[i] saved in reg for cmp 
 									; /!\ RBX, RBP, and R12 through R15 are callee-saved so thy must be save dif used
 									; /!\ we are manipulating bytes and not int (4bytes) which is the default size.
 										;So when manipulating the data we need to specify that => use movzx adds the required zeroes to fill the 64bits register
-	cmp		r10b, byte [rdi + rax];  cmp s1[i] and s2[i]
+	cmp		r8b, byte [rdi + rax];  cmp s1[i] and s2[i]
 	jne		.return; if != goto .return
-	cmp		r10b, 0; else test if end of string reached
+	cmp		r8b, 0; else test if end of string reached
 	je		.return; if so then .return
 	inc		rax; inc i
 	jmp		.cmp_char
 
 .return:
 	movzx rax, byte [rdi + rax]; return = s1[i]  /!\ still need to specify that we manipulate one byte
-	sub byte rax, byte r10; return -= s2[i]
+	sub byte rax, byte r8; return -= s2[i]
 	;epilogue
 	pop rbp
 	ret
