b93112fd980e7f118e60438284564ce97247e2c9
Alex Cardona <42.alex.cardona@gmail.com>
2025-03-04 23:55:10 +0100

    fix: error missing arg

--- tests/_bonus/test_list_display.c
+++ tests/_bonus/test_list_display.c
@@ -1,71 +1,71 @@
 #include "test_bonus.h"
 
-static void _print_lst_elem_ptr(t_list *lst)
+static void _print_lst_elem_ptr(bool print_stdout, t_list *lst)
 {
-	logged_printf(true, "%p:\n  -data: %p\n  -next: %p\n", lst, lst->data, lst->next);
+	logged_printf(print_stdout, "%p:\n  -data: %p\n  -next: %p\n", lst, lst->data, lst->next);
 }
 
-static void _print_lst_elem_str(t_list *lst)
+static void _print_lst_elem_str(bool print_stdout, t_list *lst)
 {
-	logged_printf(true, "%p:\n  -data: \"%s\"\n  -next: %p\n", lst, (char*)lst->data, lst->next);
+	logged_printf(print_stdout, "%p:\n  -data: \"%s\"\n  -next: %p\n", lst, (char*)lst->data, lst->next);
 }
 
-static void _print_lst_elem_int(t_list *lst)
+static void _print_lst_elem_int(bool print_stdout, t_list *lst)
 {
-	logged_printf(true, "%p:\n  -data: %d\n  -next: %p\n", lst, *(int *)lst->data, lst->next);
+	logged_printf(print_stdout, "%p:\n  -data: %d\n  -next: %p\n", lst, *(int *)lst->data, lst->next);
 }
 
-static void _print_lst_elem_int_silent(t_list *lst)
+static void _print_lst_elem_int_silent(bool print_stdout, t_list *lst)
 {
-	logged_printf(true, "%d", *(int *)lst->data);
+	logged_printf(print_stdout, "%d", *(int *)lst->data);
 }
 
-static void _test_print_lst_typed(t_list *lst_head, void (*print_lst_elem)(t_list *lst))
+static void _test_print_lst_typed(bool print_stdout, t_list *lst_head, void (*print_lst_elem)(bool print_stdout, t_list *lst))
 {
 	t_list *elem;
 
-	logged_printf(true, "--List--\nStart at %p\n", lst_head);
+	logged_printf(print_stdout, "--List--\nStart at %p\n", lst_head);
 	elem = lst_head;
 	while (elem)
 	{
-		(*print_lst_elem)(elem);
+		(*print_lst_elem)(print_stdout, elem);
 		elem = elem->next;
 	}
-	logged_printf(true, "--End list--\n");
+	logged_printf(print_stdout, "--End list--\n");
 }
 
 
-static void _test_print_lst_typed_silent(t_list *lst_head, void (*print_lst_elem)(t_list *lst))
+static void _test_print_lst_typed_silent(bool print_stdout, t_list *lst_head, void (*print_lst_elem)(bool print_stdout, t_list *lst))
 {
 	t_list *elem;
 
-	logged_printf(true, "[");
+	logged_printf(print_stdout, "[");
 	elem = lst_head;
 	while (elem)
 	{
-		(*print_lst_elem)(elem);
+		(*print_lst_elem)(print_stdout, elem);
 		elem = elem->next;
-		logged_printf(true, "%s", elem ? ", " : "");
+		logged_printf(print_stdout, "%s", elem ? ", " : "");
 	}
-	logged_printf(true, "]\n");
+	logged_printf(print_stdout, "]\n");
 }
 
-void test_print_lst(t_list *lst_head)
+void test_print_lst(bool print_stdout, t_list *lst_head)
 {
-	_test_print_lst_typed(lst_head, _print_lst_elem_ptr);
+	_test_print_lst_typed(print_stdout, lst_head, _print_lst_elem_ptr);
 }
 
-void test_print_lst_str(t_list *lst_head)
+void test_print_lst_str(bool print_stdout, t_list *lst_head)
 {
-	_test_print_lst_typed(lst_head, _print_lst_elem_str);
+	_test_print_lst_typed(print_stdout, lst_head, _print_lst_elem_str);
 }
 
-void test_print_lst_int(t_list *lst_head)
+void test_print_lst_int(bool print_stdout, t_list *lst_head)
 {
-	_test_print_lst_typed(lst_head, _print_lst_elem_int);
+	_test_print_lst_typed(print_stdout, lst_head, _print_lst_elem_int);
 }
 
-void test_print_lst_int_silent(t_list *lst_head)
+void test_print_lst_int_silent(bool print_stdout, t_list *lst_head)
 {
-	_test_print_lst_typed_silent(lst_head, _print_lst_elem_int_silent);
+	_test_print_lst_typed_silent(print_stdout, lst_head, _print_lst_elem_int_silent);
 }
