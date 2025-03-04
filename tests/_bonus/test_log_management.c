7a5916fbe972b5793001a682a9b3367fcebfb520
Alex Cardona <42.alex.cardona@gmail.com>
2025-03-05 00:30:17 +0100

    fix: log file was not truncated

--- tests/_bonus/test_log_management.c
+++ tests/_bonus/test_log_management.c
@@ -4,7 +4,7 @@
 char	log_file_name[128];
 int		log_file_fd = -1;
 
-void	logged_printf(bool printf_console, const char *format, ...)
+void	logged_printf(bool print_stdout, const char *format, ...)
 {
 	va_list	args;
 
@@ -14,7 +14,7 @@ void	logged_printf(bool printf_console, const char *format, ...)
 		vdprintf(log_file_fd, format, args);
 		va_end(args);
 	}
-	if (printf_console)
+	if (print_stdout)
 	{
 		va_start(args, format);
 		vprintf(format, args);
@@ -28,7 +28,7 @@ void	open_log_file(char *new_log_file_name)
 	{
 		strncpy(log_file_name, new_log_file_name, 127);
 		log_file_name [127] = 0;
-		log_file_fd = open(log_file_name, O_CREAT | O_RDWR, 0666);
+		log_file_fd = open(log_file_name, O_CREAT | O_RDWR | O_TRUNC, 0666);
 	}
 	if (log_file_fd == -1)
 		printf("WARNING:\nFAIL TO OPEN THE LOG FILE\n\n");
